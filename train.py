7e49174d2847f27f317cd62bdf071e3fbee1710a
huypham47 <huy.pp194300@sis.hust.edu.vn>
2023-01-04 20:39:45 +0700

    project

--- train.py
+++ train.py
@@ -3,8 +3,8 @@ import pandas as pd
 from pathlib import Path
 import os.path
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 
 # Load images
@@ -12,6 +12,7 @@ image_dir = Path('D:/chuyen_nganh/20221/machine_learning/project/Nhap_mom_ML-hao
 filepaths = pd.Series(list(image_dir.glob(r'**/*.jpg')), name='Filepath').astype(str)
 ages = pd.Series(filepaths.apply(lambda x: os.path.split(os.path.split(x)[0])[1]), name='Age').astype(np.int)
 images = pd.concat([filepaths, ages], axis=1).sample(frac=1.0, random_state=1).reset_index(drop=True)
+
 train_df, test_df = train_test_split(images, train_size=0.7, shuffle=True, random_state=1)
 
 # Create ImageDataGenerator
@@ -26,39 +27,63 @@ test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
 # Change iamges to dataframe
 train_images = train_generator.flow_from_dataframe(
     dataframe=train_df,
+    directory=None,
     x_col='Filepath',
     y_col='Age',
+    weight_col=None,
     target_size=(120, 120),
     color_mode='rgb',
+    classes=None,
     class_mode='raw',
     batch_size=64,
     shuffle=True,
     seed=42,
-    subset='training'
+    save_to_dir=None,
+    save_prefix='',
+    save_format='png',
+    subset='training',
+    interpolation='nearest',
+    validate_filenames=True,
 )
 
 val_images = train_generator.flow_from_dataframe(
     dataframe=train_df,
+    directory=None,
     x_col='Filepath',
     y_col='Age',
+    weight_col=None,
     target_size=(120, 120),
     color_mode='rgb',
+    classes=None,
     class_mode='raw',
     batch_size=64,
     shuffle=True,
     seed=42,
-    subset='validation'
+    save_to_dir=None,
+    save_prefix='',
+    save_format='png',
+    subset='validation',
+    interpolation='nearest',
+    validate_filenames=True,
 )
 
 test_images = test_generator.flow_from_dataframe(
     dataframe=test_df,
+    directory=None,
     x_col='Filepath',
     y_col='Age',
+    weight_col=None,
     target_size=(120, 120),
     color_mode='rgb',
+    classes=None,
     class_mode='raw',
     batch_size=64,
-    shuffle=False
+    shuffle=False,
+    save_to_dir=None,
+    save_prefix='',
+    save_format='png',
+    interpolation='nearest',
+    validate_filenames=True,
 )
 
 # Create Model
@@ -83,7 +108,7 @@ model.compile(
 history = model.fit(
     train_images,
     validation_data=val_images,
-    epochs=120
+    epochs=74
 )
 
 # Save Model
@@ -92,9 +117,6 @@ model.save('models8')
 # Predict test images
 predicted_ages = np.squeeze(model.predict(test_images))
 true_ages = test_images.labels
-
-rmse = np.sqrt(model.evaluate(test_images, verbose=0))
-print("     Test RMSE: {:.5f}".format(rmse))
 
 r2 = r2_score(true_ages, predicted_ages)
 print("Test R^2 Score: {:.5f}".format(r2))
